be0dfebc91de16dfac6d442d824719d7ce4161d0
phoenixIS <savchira9@gmail.com>
2017-07-11 19:07:05 +0300

    Add Some Tests

--- Sorting.Tests/ArraySortingTests.cs
+++ Sorting.Tests/ArraySortingTests.cs
@@ -8,17 +8,22 @@ namespace Sorting.Tests
     [TestFixture]
     public class ArraySortingTests
     {
-        private static IEnumerable<TestCaseData> TestData
+        private static int[][] TestData => new[]
+        {
+            new[] {1, 3, 21, -11, -5},
+            new[] {-22, 10, -3},
+            new[] {15, 43},
+            new[] {3, -9, 20, 18}
+        };
+
+        #region Sum In Rows
+
+        private static IEnumerable<TestCaseData> SumTestData
         {
             get
             {
-                yield return new TestCaseData(new[]
-                {
-                    new[] {1, 3, 21, -11, -5},
-                    new[] {-22, 10, -3},
-                    new[] {15, 43},
-                    new[] {3, -9, 20, 18}
-                }, new[]
+                yield return new TestCaseData(TestData,
+                new[]
                 {
                     new[] {-22, 10, -3},
                     new[] {1, 3, 21, -11, -5},
@@ -28,20 +33,81 @@ namespace Sorting.Tests
             }
         }
 
-        #region Sum In Rows
-
-        [Test, TestCaseSource(nameof(TestData))]
+        [Test, TestCaseSource(nameof(SumTestData))]
         public void Sort_Array_ArraySortedByRowsAscendingSum(int[][] array, int[][] sortedArray)
         {
             ArraySorting.Sort(array, ComparerType.SumInRows);
-            IStructuralEquatable structEq = array;
             CheckArraysForEquality(array, sortedArray);
         }
 
-        [Test, TestCaseSource(nameof(TestData))]
+        [Test, TestCaseSource(nameof(SumTestData))]
         public void Sort_Array_ArraySortedByRowsDescendingSum(int[][] array, int[][] sortedArray)
         {
             ArraySorting.Sort(array, ComparerType.SumInRows, false);
+            Array.Reverse(sortedArray);
+            CheckArraysForEquality(array, sortedArray);
+        }
+
+        #endregion
+
+        #region Max In Rows
+
+        private static IEnumerable<TestCaseData> MaxTestData
+        {
+            get
+            {
+                yield return new TestCaseData(TestData, new[]
+                {
+                    new[] {-22, 10, -3},
+                    new[] {3, -9, 20, 18},
+                    new[] {1, 3, 21, -11, -5},
+                    new[] {15, 43}
+                });
+            }
+        }
+
+        [Test, TestCaseSource(nameof(MaxTestData))]
+        public void Sort_Array_ArraySortedByRowsAscendingMax(int[][] array, int[][] sortedArray)
+        {
+            ArraySorting.Sort(array, ComparerType.MaxInRows);
+            CheckArraysForEquality(array, sortedArray);
+        }
+        [Test, TestCaseSource(nameof(MaxTestData))]
+        public void Sort_Array_ArraySortedByRowsDescendingMax(int[][] array, int[][] sortedArray)
+        {
+            ArraySorting.Sort(array, ComparerType.MaxInRows, false);
+            Array.Reverse(sortedArray);
+            CheckArraysForEquality(array, sortedArray);
+        }
+
+        #endregion
+
+        #region Min In Rows
+
+        private static IEnumerable<TestCaseData> MinTestData
+        {
+            get
+            {
+                yield return new TestCaseData(TestData, new[]
+                {
+                    new[] {-22, 10, -3},
+                    new[] {1, 3, 21, -11, -5},
+                    new[] {3, -9, 20, 18},
+                    new[] {15, 43}
+                });
+            }
+        }
+
+        [Test, TestCaseSource(nameof(MinTestData))]
+        public void Sort_Array_ArraySortedByRowsAscendingMin(int[][] array, int[][] sortedArray)
+        {
+            ArraySorting.Sort(array, ComparerType.MinInRows);
+            CheckArraysForEquality(array, sortedArray);
+        }
+        [Test, TestCaseSource(nameof(MinTestData))]
+        public void Sort_Array_ArraySortedByRowsDescendingMin(int[][] array, int[][] sortedArray)
+        {
+            ArraySorting.Sort(array, ComparerType.MinInRows, false);
             Array.Reverse(sortedArray);
             CheckArraysForEquality(array, sortedArray);
         }
